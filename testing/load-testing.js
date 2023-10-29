2814d9d54a27140215c3280d8f6b8917d0d29e89
muhammad.haqqi01 <muhammad.haqqi01@ui.ac.id>
2023-10-29 19:17:37 +0700

    feat: updated files & folders naming, updated chaos & load scripts

--- testing/load-testing.js
+++ testing/load-testing.js
@@ -3,22 +3,30 @@ import { check, sleep } from 'k6';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.4/index.js';
 
 export const options = {
+  vus: 10,
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
+    // { duration: '30s', target: 20 },
+    // { duration: '1m30s', target: 10 },
+    { duration: '30s', target: 10 },
+    { duration: '30s', target: 0 },
   ],
 };
 
 export default function () {
-  const res = http.get('https://httpbin.test.k6.io/');
+  // const res = http.get('http://138.197.231.156:8000/health');
+
+  let data = {
+    "type": "text",
+    "text": "What german name of apple"
+  };
+  const res = http.post('http://138.197.231.156:8000/inference', JSON.stringify(data))
+
   check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
+  sleep(15);
 }
 
 export function handleSummary(data) {
   return {
-    "log/load-testing-steadystate.log": textSummary(data, { indent: '→', enableColors: false }),
+    [`log/running/load-testing-${__ENV.CHAOS_TYPE}.log`]: textSummary(data, { indent: '→', enableColors: false })
   };
 }
-
--- testing/run-experiment.sh
+++ testing/run-experiment.sh
@@ -1,6 +1,58 @@
 #!/bin/bash
-CURRENTDATE=`date +"%d-%m-%Y"`
 
+# Echo
+echo "-----------------------------------------------------------------------------"
+echo "Experiment has been start!"
+echo -e "Semoga eksperimen yang dilakukan akan berjalan dengan lancar aamiin\n"
+
+# Init variables
+CURRENTDATE=`date +"%d-%m-%Y-%H:%M"`
+CHAOS_DATE="(${CURRENTDATE})"
+
+# Init variables for directories
+CHAOS_RUNNING_LOG_DIR="log/running"
+CHAOS_FINAL_LOG_DIR="log/${CURRENTDATE}"
+CHAOS_REPORT_LOG_DIR="report/${CURRENTDATE}"
+
+# Init variables for directory files
+CHAOS_EXPERIMENT_LOG_FILES_DIR="${CHAOS_RUNNING_LOG_DIR}/chaos-experiment.log"
+CHAOS_PROXY_REPORT_FILES_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-proxy-journal.json"
+CHAOS_COMBINED_REPORT_FILES_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-report.pdf"
+
+# Init commands before testing
+rm -r "${CHAOS_RUNNING_LOG_DIR}"
+mkdir "${CHAOS_RUNNING_LOG_DIR}"
+
+# Run load testing
+export CHAOS_TYPE="steadystate"
 k6 run load-testing.js
-chaos run experiment.json --journal-path "report/chaos-journal-(${CURRENTDATE}).json" --hypothesis-strategy before-method-only
-chaos report --export-format=pdf "report/chaos-journal-(${CURRENTDATE}).json" "report/chaos-report-(${CURRENTDATE}).pdf"
+
+# Init commands before chaos experiment
+mkdir "${CHAOS_REPORT_LOG_DIR}"
+
+# Run chaos experiment: turn off interface-proxy
+# gcloud compute instances stop interface-proxy
+# sleep 5
+export CHAOS_TYPE="off-proxy"
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILES_DIR}" run experiment/off-proxy.json --journal-path "${CHAOS_PROXY_REPORT_FILES_DIR}" --hypothesis-strategy before-method-only
+# gcloud compute instances start interface-proxy
+# sleep 5
+
+# Run chaos experiment: turn off event-receiver
+
+# Run chaos experiment: turn off MaaS
+
+
+# Run chaos experiment: turn off ML Pipeline
+
+# Create reports from chaos experiments
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILES_DIR}" report --export-format=pdf "${CHAOS_PROXY_REPORT_FILES_DIR}" "${CHAOS_COMBINED_REPORT_FILES_DIR}"
+
+# Move all log from log/running to log/${date}
+rm -r "${CHAOS_FINAL_LOG_DIR}"
+mkdir "${CHAOS_FINAL_LOG_DIR}"
+mv "${CHAOS_RUNNING_LOG_DIR}"/* "${CHAOS_FINAL_LOG_DIR}"
+rm -r "${CHAOS_RUNNING_LOG_DIR}"
+
+echo -e "\nExperiment has been done!"
+echo "-----------------------------------------------------------------------------"
