c556dec21c5adc2b88eb6648049e3d15d22d0512
muhammad.haqqi01 <muhammad.haqqi01@ui.ac.id>
2023-11-25 17:30:58 +0700

    feat: finished testing script

--- testing/load-testing.js
+++ testing/load-testing.js
@@ -1,25 +1,23 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js'
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.4/index.js';
 
 export const options = {
-  vus: 10,
+  vus: 0,
   stages: [
-    // { duration: '30s', target: 20 },
-    // { duration: '1m30s', target: 10 },
-    { duration: '30s', target: 10 },
-    { duration: '30s', target: 0 },
+    { duration: '30s', target: 600 },
+    { duration: '120s', target: 600 },
+    { duration: '30s', target: 0 }
   ],
 };
 
-export default function () {
-  // const res = http.get('http://138.197.231.156:8000/health');
+const DATASETS = new SharedArray('some data name', function () {
+  return JSON.parse(open('./dataset/combined-dataset-55k.json'));
+});
 
-  let data = {
-    "type": "text",
-    "text": "What german name of apple"
-  };
-  const res = http.post('http://138.197.231.156:8000/inference', JSON.stringify(data))
+export default function () {
+  const res = http.post('http://35.208.32.246:8000/inference', JSON.stringify(randomItem(DATASETS)))
 
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(15);
--- testing/run-experiment.sh
+++ testing/run-experiment.sh
@@ -11,12 +11,21 @@ CHAOS_DATE="(${CURRENTDATE})"
 
 # Init variables for directories
 CHAOS_RUNNING_LOG_DIR="log/running"
+CHAOS_EXPERIMENT_LOG_DIR="experiment"
 CHAOS_FINAL_LOG_DIR="log/${CURRENTDATE}"
 CHAOS_REPORT_LOG_DIR="report/${CURRENTDATE}"
 
 # Init variables for directory files
-CHAOS_EXPERIMENT_LOG_FILES_DIR="${CHAOS_RUNNING_LOG_DIR}/chaos-experiment.log"
-CHAOS_PROXY_REPORT_FILES_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-proxy-journal.json"
+CHAOS_INTERFACE_TEST_FILE_DIR="${CHAOS_EXPERIMENT_LOG_DIR}/off-interface.json"
+CHAOS_INFERENCE_TEST_FILE_DIR="${CHAOS_EXPERIMENT_LOG_DIR}/off-inference.json"
+CHAOS_TRAINING_TEST_FILE_DIR="${CHAOS_EXPERIMENT_LOG_DIR}/off-training.json"
+
+CHAOS_EXPERIMENT_LOG_FILE_DIR="${CHAOS_RUNNING_LOG_DIR}/chaos-experiment.log"
+
+CHAOS_INTERFACE_REPORT_FILE_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-interface-journal.json"
+CHAOS_INFERENCE_REPORT_FILE_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-inference-journal.json"
+CHAOS_TRAINING_REPORT_FILE_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-training-journal.json"
+
 CHAOS_COMBINED_REPORT_FILES_DIR="${CHAOS_REPORT_LOG_DIR}/chaos-report.pdf"
 
 # Init commands before testing
@@ -24,20 +33,20 @@ rm -r "${CHAOS_RUNNING_LOG_DIR}"
 mkdir "${CHAOS_RUNNING_LOG_DIR}"
 
 # Run load testing
-export CHAOS_TYPE="steadystate"
 k6 run load-testing.js
 
 # Init commands before chaos experiment
 mkdir "${CHAOS_REPORT_LOG_DIR}"
 
-# Run chaos experiment: turn off interface-proxy
-chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILES_DIR}" run experiment/off-proxy.json --journal-path "${CHAOS_PROXY_REPORT_FILES_DIR}" --hypothesis-strategy before-method-only
-# Run chaos experiment: turn off event-receiver
-# Run chaos experiment: turn off MaaS
-# Run chaos experiment: turn off ML Pipeline
+# Run chaos experiment: turn off interface
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILE_DIR}" run "${CHAOS_INTERFACE_TEST_FILE_DIR}" --journal-path "${CHAOS_INTERFACE_REPORT_FILE_DIR}" --hypothesis-strategy before-method-only
+# Run chaos experiment: turn off ML Inference
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILE_DIR}" run "${CHAOS_INFERENCE_TEST_FILE_DIR}" --journal-path "${CHAOS_INFERENCE_REPORT_FILE_DIR}" --hypothesis-strategy before-method-only
+# Run chaos experiment: turn off ML Training
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILE_DIR}" run "${CHAOS_TRAINING_TEST_FILE_DIR}" --journal-path "${CHAOS_TRAINING_REPORT_FILE_DIR}" --hypothesis-strategy before-method-only
 
 # Create reports from chaos experiments
-chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILES_DIR}" report --export-format=pdf "${CHAOS_PROXY_REPORT_FILES_DIR}" "${CHAOS_COMBINED_REPORT_FILES_DIR}"
+chaos --log-file "${CHAOS_EXPERIMENT_LOG_FILE_DIR}" report --export-format=pdf "${CHAOS_INTERFACE_REPORT_FILE_DIR}" "${CHAOS_INFERENCE_REPORT_FILE_DIR}" "${CHAOS_TRAINING_REPORT_FILE_DIR}""${CHAOS_COMBINED_REPORT_FILES_DIR}"
 
 # Move all log from log/running to log/${date}
 rm -r "${CHAOS_FINAL_LOG_DIR}"
