4175d5a7016272b18eadd9e06bdf389e15bbdf37
muhammad.haqqi01 <muhammad.haqqi01@ui.ac.id>
2023-11-30 19:43:07 +0700

    feat: make sure that data sent ratio of txt:txi is 10:1

--- testing/load-testing.js
+++ testing/load-testing.js
@@ -2,6 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js'
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.4/index.js';
+import exec from 'k6/execution';
 
 export const options = {
   vus: 0,
@@ -12,12 +13,19 @@ export const options = {
   ],
 };
 
-const DATASETS = new SharedArray('some data name', function () {
+const DATASETS = new SharedArray('prompts', function () {
   return JSON.parse(open('./dataset/combined-dataset-55k.json'));
 });
 
 export default function () {
-  const res = http.post('http://35.208.32.246:8000/inference', JSON.stringify(randomItem(DATASETS)))
+  const { vu } = exec;
+  let data = {}
+
+  if (vu.iterationInInstance % 600 < 540)
+    data = DATASETS[Math.floor(Math.random() * 50000)]
+  else data = DATASETS[Math.floor(Math.random() * 5000 + 50000)]
+
+  const res = http.post('http://35.208.32.246:8000/inference', JSON.stringify(data))
 
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(15);
