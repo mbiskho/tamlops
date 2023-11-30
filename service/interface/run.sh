3588bfcf88b1b5e1a683d48c92c61b4ef386deec
muhammad.haqqi01 <muhammad.haqqi01@ui.ac.id>
2023-11-30 10:54:55 +0700

    fix: node-exporter in interface is not turn on

--- service/interface/run.sh
+++ service/interface/run.sh
@@ -3,7 +3,7 @@
 # run prometheus + node-exporter
 systemctl daemon-reload
 systemctl enable node_exporter
-systemctl enable node_exporter
+systemctl start node_exporter
 systemctl enable prometheus
 systemctl start prometheus
 
