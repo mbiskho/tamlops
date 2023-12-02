ad9aa8983118697b679a76a44c550dd3c66b8056
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-02 15:42:02 +0700

    feat: add text to image benchmark

--- service/interface/modules/schedule.py
+++ service/interface/modules/schedule.py
@@ -12,8 +12,12 @@ async def schedule_logic():
     tasks = await get_from_db()
 
     # Load the linear regression model from the .pkl file
-    with open('./models/text_scheduler_gpu_model.pkl', 'rb') as file:
-        model = pickle.load(file)
+    with open('./models/text_to_text_model.pkl', 'rb') as file:
+        model_text = pickle.load(file)
+
+    # Load the linear regression model from the .pkl file
+    with open('./models/text_to_image_model.pkl', 'rb') as file:
+        model_image = pickle.load(file)
 
     # List to store tasks with their estimated times
     tasks_with_times = []
@@ -22,33 +26,67 @@ async def schedule_logic():
         # Parse the JSON string to a dictionary
         params_dict = json.loads(task['params'])
 
-        new_features = pd.DataFrame({
-            'num_train_epochs': [params_dict['num_train_epochs']],
-            'learning_rate': [params_dict['learning_rate']],
-            'per_device_eval_batch_size': [params_dict['per_device_eval_batch_size']],
-            'per_device_train_batch_size': [params_dict['per_device_train_batch_size']],
-            'file_size (bytes)': [task['size']]
-        })
+        if task['type'] == 'text':
+            new_features = pd.DataFrame({
+                'num_train_epochs': [params_dict['num_train_epochs']],
+                'learning_rate': [params_dict['learning_rate']],
+                'per_device_eval_batch_size': [params_dict['per_device_eval_batch_size']],
+                'per_device_train_batch_size': [params_dict['per_device_train_batch_size']],
+                'file_size (bytes)': [task['size']]
+            })
 
-        # Use the loaded model to predict the target variable for the new feature values
-        predicted_time_raw = model.predict(new_features)
-        
-        predicted_metric = np.maximum(predicted_time_raw, 0)
+            # Use the loaded model to predict the target variable for the new feature values
+            predicted_time_raw = model_text.predict(new_features)
+            
+            predicted_metric = np.maximum(predicted_time_raw, 0)
 
-        # Append task with its predicted time to the list and restructure it
-        tasks_with_times.append({
-            "file": task['file'],
-            "type": task['type'],
-            "id": task['id'],
-            "estimated_time": predicted_metric[0][0],
-            "gpu_usage": predicted_metric[0][1],
-            "param": {
-                "per_device_train_batch_size": params_dict['per_device_train_batch_size'],
-                "per_device_eval_batch_size": params_dict['per_device_eval_batch_size'],
-                "learning_rate": params_dict['learning_rate'],
-                "num_train_epochs": params_dict['num_train_epochs']
-            }
-        })
+            # Append task with its predicted time to the list and restructure it
+            tasks_with_times.append({
+                "file": task['file'],
+                "type": task['type'],
+                "id": task['id'],
+                "estimated_time": predicted_metric[0][0],
+                "gpu_usage": predicted_metric[0][1],
+                "param": {
+                    "per_device_train_batch_size": params_dict['per_device_train_batch_size'],
+                    "per_device_eval_batch_size": params_dict['per_device_eval_batch_size'],
+                    "learning_rate": params_dict['learning_rate'],
+                    "num_train_epochs": params_dict['num_train_epochs']
+                }
+            })
+        elif task['type'] == 'image':
+            new_features = pd.DataFrame({
+                'resolution': params_dict['resolution'],
+                'train_batch_size': params_dict['train_batch_size'],
+                'num_train_epochs': params_dict['num_train_epochs'],
+                'max_train_steps': params_dict['max_train_steps'],
+                'learning_rate': params_dict['learning_rate'],
+                'gradient_accumulation_steps': params_dict['gradient_accumulation_steps'],
+                'file_size': params_dict['file_size']
+            })
+
+             # Use the loaded model to predict the target variable for the new feature values
+            predicted_time_raw = model_image.predict(new_features)
+            
+            predicted_metric = np.maximum(predicted_time_raw, 0)
+
+             # Append task with its predicted time to the list and restructure it
+            tasks_with_times.append({
+                    "file": task['file'],
+                    "type": task['type'],
+                    "id": task['id'],
+                    "estimated_time": predicted_metric[0][0],
+                    "gpu_usage": predicted_metric[0][1],
+                    "param": {
+                    'resolution': params_dict['resolution'],
+                    'train_batch_size': params_dict['train_batch_size'],
+                    'num_train_epochs': params_dict['num_train_epochs'],
+                    'max_train_steps': params_dict['max_train_steps'],
+                    'learning_rate': params_dict['learning_rate'],
+                    'gradient_accumulation_steps': params_dict['gradient_accumulation_steps'],
+                }
+            })
+
 
     # Sort tasks based on estimated time (from least to longest)
     sorted_tasks = sorted(tasks_with_times, key=lambda x: x['estimated_time'])
