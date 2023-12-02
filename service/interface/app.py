2920033786cbed75f37ce1e18925a1e858f31863
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-02 19:21:40 +0700

    feat: add await;

--- service/interface/app.py
+++ service/interface/app.py
@@ -58,6 +58,6 @@ async def schedule():
     
 @app.get('/schedule/fcfs', response_class=JSONResponse)
 async def schedule():
-    schedule_logic_fcfs()
+    await schedule_logic_fcfs()
     return {"error": False, "response": "Schedule Started"}
 
