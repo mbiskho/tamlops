862ba3565bfcb4c0621409daf084c44896cdf32d
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-03 19:16:06 +0700

    fix algo

--- service/interface/modules/algo.py
+++ service/interface/modules/algo.py
@@ -6,14 +6,14 @@ def allocate_gpu(tasks, gpus):
     print(gpus)
 
     # Create a min heap based on memory_usage using heapq
-    min_heap = [(gpu['memory_used'], gpu) for gpu in gpus]
+    min_heap = [(gpu['memory_used'], gpu['index']) for gpu in gpus]
     heapq.heapify(min_heap)
 
     for task in tasks:
         print(min_heap)
         smallest_memory_usage, gpu = heapq.heappop(min_heap)
         heapq.heappush(min_heap, (smallest_memory_usage + task['gpu_usage'], gpu))
-        task['num_gpu'] = gpu['index']
+        task['num_gpu'] = gpu
         del task['estimated_time']
         del task['gpu_usage']
         task_sorted.append(task)
