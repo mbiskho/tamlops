75daf81791a52d5f3125abd8dfbfbb8acda92267
muhammad.haqqi01 <muhammad.haqqi01@ui.ac.id>
2023-12-03 14:03:37 +0700

    feat: set prometheus scrape to 15s

--- service/pipeline/install-prom-node.sh
+++ service/pipeline/install-prom-node.sh
@@ -26,6 +26,21 @@ mv consoles /etc/prometheus
 mv console_libraries /etc/prometheus
 mv prometheus.yml /etc/prometheus
 
+
+echo "global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: 'node'
+    static_configs:
+      - targets: ['localhost:9100']
+
+remote_write:
+  - url: 'https://prometheus-prod-18-prod-ap-southeast-0.grafana.net/api/prom/push'
+    basic_auth:
+      username: '1193718'
+      password: 'glc_eyJvIjoiOTQ2OTQwIiwibiI6InN0YWNrLTc0MjkwMS1obS13cml0ZS1ub2RlLWV4cG9ydGVyIiwiayI6IjlkRUZOajg1WUQ2T1cwMjJjVkU2dGk3MiIsIm0iOnsiciI6InByb2QtYXAtc291dGhlYXN0LTAifX0='"  >> /etc/prometheus/prometheus.yml
+
 chown prometheus:prometheus /etc/prometheus
 chown -R prometheus:prometheus /etc/prometheus/consoles
 chown -R prometheus:prometheus /etc/prometheus/console_libraries
