c9db65eaec146154fff6ed6d937603190e9b7b31
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-04 17:07:35 +0700

    remove print

--- service/interface/modules/schedule.py
+++ service/interface/modules/schedule.py
@@ -283,7 +283,6 @@ async def schedule_logic_fcfs_burst(gpu_id):
             print(task)
             params_dict = json.loads(task['params'])
             print(task['id'])
-            print(params_dict['per_device_train_batch_size'])
 
             url = "http://127.0.0.1:6070/train-burst"
 
