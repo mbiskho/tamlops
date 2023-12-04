3ce218d5ab7291177d867c2e18d308f09bae7fa8
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-04 19:57:07 +0700

    feat: add error logs

--- service/interface/app.py
+++ service/interface/app.py
@@ -46,11 +46,8 @@ async def schedule():
     
 @app.get('/schedule/max-min', response_class=JSONResponse)
 async def schedule():
-    try:
-        post_response = await schedule_logic_max_min()
-        return post_response
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"error": str(e)})
+    post_response = await schedule_logic_max_min()
+    return {"error": False, "response": post_response}
     
 @app.get('/schedule/fcfs-burst/{gpu_id}', response_class=JSONResponse)
 async def schedule(gpu_id: str):
