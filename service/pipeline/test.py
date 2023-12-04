f72ab5656ba3095c9572b4cd4dc51d750df7bd86
mbiskho <khomeini.bisma@gmail.com>
2023-12-04 12:51:01 +0700

    duar-1

--- service/pipeline/test.py
+++ service/pipeline/test.py
@@ -12,4 +12,22 @@ command = [
             f'8012'
 ]
 
+# command = [
+#             'sh', 
+#             'image.sh', 
+#             f'1'
+#             f'10', 
+#             f'1', 
+#             f'1', 
+#             f'2', 
+#             f'0.001', 
+#             f'1', 
+#             f'https://storage.googleapis.com/training-dataset-tamlops/train-00000-of-00001-566cc9b19d7203f8.parquet',
+#             f'11293'
+# ]
+
 result = subprocess.run(command, capture_output=True, text=True, check=True)
+print("Subprocess output (stdout):", result.stdout)
+print("Subprocess output (stderr):", result.stderr)
+
+
