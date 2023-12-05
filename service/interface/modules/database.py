51f6f4357a5ac3d9800911d2021b051587f3cbbe
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-12-06 04:10:33 +0700

    feat: remove print

--- service/interface/modules/database.py
+++ service/interface/modules/database.py
@@ -5,7 +5,6 @@ import json
 DB = "postgres://postgres.obxofwxgpggksbvuzzfn:tamlops123!@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
 
 async def save_training_db(type, file, size, params):
-    print(DB)
     conn = await asyncpg.connect(DB)
     await conn.execute("INSERT INTO training_queue (type, file, size, params) VALUES ($1, $2, $3, $4)", type, file, size, params)
     await conn.close()
@@ -26,7 +25,6 @@ async def get_from_db():
     return fetched_rows
 
 async def delete_all_from_table():
-    print(DB)
     conn = await asyncpg.connect(DB)
     try:
         await conn.execute("DELETE FROM training_queue")  # Replace 'your_table_name' with your actual table name
